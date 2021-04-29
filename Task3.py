3090766146e37cd03f23b57a4d005cf6e808d5ef
AlexanderAdel <alexander_adel@hotmail.com>
2021-04-29 21:44:34 +0200

    Task3.

---
+++ Task3.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import Task2
+
+import sys
+import numpy # necessary for updates
+
+import jax
+import jax.numpy as np
+import jax.config
+
+jax.config.update("jax_enable_x64", True)
+
+
+def readInputArguments(argv):
+	if len(argv) == 4:
+		scipt, path, delta_t, N = argv
+		return path, float(delta_t), int(N)
+	else:
+		print("Usage: ./Task3 path delta_t N")
+		exit()
+
+    
+def readFile(path):
+    f = open(path, "r")
+    M = int(f.readline())
+    C = f.readline()
+    L = float(f.readline())
+
+    coords = []
+    vels = []
+    for line in f:
+        coords.append([float(x) for x in line.split()[0:3]])
+        vels.append([float(x) for x in line.split()[3:6]])
+    f.close()
+
+    return M, C, L, np.array(coords), np.array(vels)
+
+
+def calculateNewCoords(coords, vels, forces, M, L, delta_t):
+    newCoords = numpy.full_like(coords, 0.0) # numpy array allows updates
+    for i in range(0, coords.shape[0]):
+        for j in [0, 1, 2]:
+            newCoords[i][j] = coords[i][j] + \
+                              vels[i][j] * delta_t + \
+                              0.5 * forces[i][j] * (delta_t)**2
+    return newCoords
+
+
+def calculateNewVels(coords, vels, forces, M, L, delta_t):
+    newForces = Task2.calculateInitialForces(M, L, coords)
+    newVels = numpy.full_like(vels, 0.0) # numpy array allows updates
+    for i in range(0, vels.shape[0]):
+        for j in [0, 1, 2]:
+            newVels[i][j] = vels[i][j] + \
+                            0.5 * (forces[i][j] + newForces[i][j]) * delta_t
+    return newVels, newForces
+
+
+def VerletAlgorithm(coords, vels, forces, M, delta_t, N):
+    filestring = Task2.createFilestring(M, L, coords, vels, "Time step 0")
+    for k in range(1, N):
+        coords = calculateNewCoords(coords, vels, forces, M, L, delta_t)
+        vels, forces = calculateNewVels(coords, vels, forces, M, L, delta_t)
+        filestring += Task2.createFilestring(M, L, coords, vels, f"Time step {k}")
+
+    Task2.writeFile(filestring, "trajectories.txt")
+
+
+if __name__ == "__main__":
+    # Read input parameters and file
+    path, delta_t, N = readInputArguments(sys.argv)
+    M, C, L, coords, vels = readFile(path)
+
+    forces = Task2.calculateInitialForces(M, L, coords)
+    VerletAlgorithm(coords, vels, forces, M, delta_t, N)
